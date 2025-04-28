5138c8db31f1002a2cebadf245596d0b93f226c1
aidan-smagh <aidansmagh@gmail.com>
2025-04-27 23:47:27 -0400

    Add functionality for admin to create new trainings

--- database/dbTrainings.php
+++ database/dbTrainings.php
@@ -19,7 +19,23 @@ function get_all_trainings() {
     mysqli_close($con);
     return $trainings;
 }
+function get_all_trainings_with_description() {
+    $con = connect();
+    $query = "SELECT name, description FROM dbTrainings ORDER BY name ASC";
+    //execute the query and store it in $result
+    $result = mysqli_query($con, $query);
+    // array to hold the trainings for return
+    $trainings = [];
 
+    // add each row of data, stored in the $result table, to the trainingss array
+    // a row in $result consists of id, title, and url
+    while ($row = mysqli_fetch_assoc($result)) {
+        $trainings[] = $row;
+    }
+    // close database connection and return the trainings array
+    mysqli_close($con);
+    return $trainings;
+}
 function update_trainings($id, $updatedTraining) {
     $con = connect();
 
@@ -62,5 +78,35 @@ function get_trainings_for($id) {
     mysqli_stmt_close($stmt);
     mysqli_close($con);
     return $trainings;
+}
+function add_training_to_db($name, $desc) {
+    $con = connect();
+    $query = 'INSERT INTO dbtrainings (name, description) VALUES (?, ?)';
+    $stmt = mysqli_prepare($con, $query);
+    mysqli_stmt_bind_param($stmt, "ss", $name, $desc);
+    $result = mysqli_stmt_execute($stmt);
+    mysqli_stmt_close($stmt);
+    mysqli_close($con);
+    return $result;
+}
+
+function delete_training($name) {
+    $con = connect();
+    /* prepare the deletion statement */
+    $stmt = $con->prepare("DELETE FROM dbtrainings WHERE name = ?");
+    $stmt->bind_param("s", $name);
+
+    /* Attempt to execute the deletion, storing the bool result in $success */
+    $success = $stmt->execute();
+    $stmt->close();
+    mysqli_close($con);
+
+    /* For error handling, return the success status */
+    if ($success) {
+        return "success";
+    }
+    else {
+        return "error";
+    }
 }
 ?>
